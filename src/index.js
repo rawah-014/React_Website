e0b60e87271c8fc5e4edbe9c6705d880e4248f00
Rawah <60169478+rawah-014@users.noreply.github.com>
2022-09-21 10:46:55 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,29 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 
+import { Provider } from 'react-redux'
+import store from './store'
+
+import { positions, transitions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic'
+
+const options = {
+  timeout: 5000,
+  position: positions.BOTTOM_CENTER,
+  transition: transitions.SCALE
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store} >
+  <AlertProvider template={AlertTemplate} {...options}>
   <React.StrictMode>
     <BrowserRouter>
         <App />
     </BrowserRouter>
   </React.StrictMode>
+  </AlertProvider>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
